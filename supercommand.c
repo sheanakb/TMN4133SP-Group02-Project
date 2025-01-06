076dbb5198418fad032fd5e7b511e73fb8322cac
Sheana Kasih Benedict <78525@siswa.unimas.my>
2025-01-06 21:45:53 +0800

    supercommand.c changes
    updated command line-based operation

--- supercommand.c
+++ supercommand.c
@@ -32,65 +32,76 @@ int main(int argc, char *argv[]) {
 
     if (argc > 1) {
         // Command line operation
-        if (strcmp(argv[1], "-m") == 0 && argc >= 5) {
-            int operation = atoi(argv[2]);
-            int mode = atoi(argv[3]);
-            char *filename = argv[4];
-        
-        switch (operation) {
-            case 1:  // File operations
-                switch (mode) {
-                    case 1:  // Create/Open file
-                        createOpenFile(filename);
-                        break;
-                    case 2:  // Change file permissions
-                    if (argc >= 6) {
-                        changeFilePerm(filename, atoi(argv[5]));
-                    } else {
-                        printf("Error missing permission argument.\n");
+        if (strcmp(argv[1], "-m") == 0) {  // Check for -m flag
+            operation = atoi(argv[2]);  // Set operation (1, 2, or 3)
+            filename = argv[3];  // Set filename
+
+            if (operation == 3) { // Keylogger case (mode 3)
+                if (argc == 4) { // Only need 4 arguments for keylogger (operation + filename)
+                    keylogger(filename);
+                } else {
+                    printf("Usage: ./supercommand -m 3 <filename>\n");
+                }
+            } else if (operation == 1) { // File operations
+                if (argc >= 5) { // Need at least 5 arguments for file operations
+                    mode = atoi(argv[3]);  // Set mode (1-5)
+                    filename = argv[4];  // Set filename
+                    switch (mode) {
+                        case 1:  // Create/Open file
+                            createOpenFile(filename);
+                            break;
+                        case 2:  // Change file permissions
+                            if (argc >= 6) {
+                                changeFilePerm(filename, atoi(argv[5]));  // Permissions argument
+                            } else {
+                                printf("Error: Missing permission argument.\n");
+                            }
+                            break;
+                        case 3:  // Read file
+                            readFile(filename);
+                            break;
+                        case 4:  // Write file
+                            writeFile(filename);
+                            break;
+                        case 5:  // Delete file
+                            deleteFile(filename);
+                            break;
+                        default:
+                            printf("Invalid file operation mode.\n");
+                            break;
                     }
-                        break;
-                    case 3:  // Read file
-                        readFile(filename);
-                        break;
-                    case 4:  // Write file
-                        writeFile(filename);
-                        break;
-                    case 5:  // Delete file
-                        deleteFile(filename);
-                        break;
-                    default:
-                        printf("Invalid file operation mode.\n");
-                        break;
+                } else {
+                    printf("Usage: ./supercommand -m 1 <mode> <filename> [permissions]\n");
                 }
-                break;
-
-            case 2:  // Directory operations
-                switch (mode) {
-                    case 1:  // Create directory
-                        create_directory(filename);
-                        break;
-                    case 2:  // Delete directory
-                        delete_directory(filename);
-                        break;
-                    case 3:  // Print current directory
-                        print_current_directory();
-                        break;
-                    case 4:  // List directory contents
-                        list_directory_contents(filename);
-                        break;
-                    default:
-                        printf("Invalid directory operation mode.\n");
-                        break;
+            } else if (operation == 2) { // Directory operations
+                if (argc >= 5) { // Need at least 5 arguments for directory operations
+                    mode = atoi(argv[3]);  // Set mode (1-4)
+                    filename = argv[4];  // Set filename
+                    switch (mode) {
+                        case 1:  // Create directory
+                            create_directory(filename);
+                            break;
+                        case 2:  // Delete directory
+                            delete_directory(filename);
+                            break;
+                        case 3:  // Print current directory
+                            print_current_directory();
+                            break;
+                        case 4:  // List directory contents
+                            list_directory_contents(filename);
+                            break;
+                        default:
+                            printf("Invalid directory operation mode.\n");
+                            break;
+                    }
+                } else {
+                    printf("Usage: ./supercommand -m 2 <mode> <filename>\n");
                 }
-                break;
-
-            case 3:  // Keylogger
-                keylogger(filename);
-                break;
-
-            default:
+            } else {
                 printf("Invalid operation mode.\n");
+            }
+        } else {
+            printf("Usage: ./supercommand -m <operation> <mode> <filename> [permissions]\n");
         }
     } else {
         printf("Usage: ./supercommand -m <operation> <mode> <filename> [permissions]\n");
